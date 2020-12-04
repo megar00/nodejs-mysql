6ab8dd3fc63889a27277256f87f6b6978d23beeb
KIKE-PI <kamaleon_3000@hotmail.com>
2020-12-03 22:25:01 -0600

    keys links bq

--- src/database/db.sql
+++ src/database/db.sql
@@ -2,9 +2,10 @@ CREATE DATABASE database_links;
 
 USE database_links;
 
+--USERS TABLE
 CREATE TABLE users(
     id INT(11) NOT NULL,
-    username VARCHAR(16) NOT NULL.
+    username VARCHAR(16) NOT NULL,
     password VARCHAR(60) NOT NULL,
     fullname VARCHAR(100) NOT NULL
 );
@@ -16,3 +17,22 @@ ALTER TABLE users
     MODIFY id INT(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT = 2;
 
 DESCRIBE users;
+
+--LINKS TABLES
+CREATE TABLE links (
+    id INT(11) NOT NULL,
+    title VARCHAR(150) NOT NULL,
+    url VARCHAR(255) NOT NULL,
+    description TEXT,
+    user_id INT(11),
+    created_at timestamp NOT NULL DEFAULT current_timestamp,
+    CONSTRAINT fk_user FOREIGN KEY (user_id) REFERENCES users(id)
+);
+
+ALTER TABLE links
+    ADD PRIMARY KEY (id);
+
+ALTER TABLE links
+        MODIFY id INT(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT = 2;
+
+DESCRIBE links;
---
+++ src/keys.js
@@ -0,0 +1,9 @@
+module.exports = {
+
+    database: {
+        host: 'localport',
+        user: 'root',
+        password: 'Alexander-098',
+        database: 'database_links'
+    }
+}
