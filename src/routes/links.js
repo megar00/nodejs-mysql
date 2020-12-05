e7bc19054b7a61e83b43b40b21320fdf3ed76353
kike-lap <kamaleon@gmail.com>
2020-12-05 01:32:57 -0600

    add formulario

--- src/routes/links.js
+++ src/routes/links.js
@@ -1,3 +1,4 @@
+const { request } = require('express');
 const express = require('express');
 const router = express.Router();
 
@@ -7,8 +8,15 @@ router.get('/add', (req, res) => {
     res.render('links/add');
 });
 
-router.post('/add', (req, res) => {
-    res.send('recived');
+router.post('/add', async (req, res) => {
+    const { title, url, description } = req.body;
+    const newLink = {
+        title,
+        url,
+        description
+    };
+    await pool.query('INSERT INTO links set ?', [newLink]);
+        res.send('recived');
 });
 
 module.exports = router;
